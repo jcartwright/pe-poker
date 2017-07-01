9f664c82c9d56cda69aa8706f6523f65c90055df
Jason Cartwright <jcartwright@Jasons-MBP.fios-router.home>
2017-07-01 16:20:14 -0500

    Implement named hands in Poker::Hand
    - #royal_flush?, #full_house?, etc.

--- lib/poker/hand.rb
+++ lib/poker/hand.rb
@@ -1,16 +1,78 @@
 class Poker::Hand
+  include Comparable
+
   VALID_CARD_COUNT = 5
 
   attr_reader :cards
 
   def initialize(cards)
+    cards = cards.split if cards.respond_to?(:split)
     @cards = parse_cards(validate_cards!(cards))
+  end
+
+  def <=>(other)
+    rank <=> other.rank
   end
 
   def face_value
     cards.reduce(0) { |sum, card|
       sum + card.weight
     }
+  end
+
+  def high_card
+    cards.max
+  end
+
+  def rank
+    # royal flush => all face (ace..ten), all same suit
+    # uniq on suit, face value of 60
+    case 
+    when royal_flush? then 100
+    else
+      high_card.weight
+    end
+  end
+
+  def royal_flush?
+    face_value == 60 && same_suit?(cards)
+  end
+
+  def straight_flush?
+    same_suit?(cards) && sequential?(cards)
+  end
+
+  def four_of_a_kind?
+    groups = card_groups_by(cards, :weight)
+    groups.any? { |weight, cards| cards.size == 4 } 
+  end
+
+  def full_house?
+    groups = card_groups_by(cards, :weight)
+    groups.map { |weight, cards| cards.size }.sort == [2,3]
+  end
+
+  def flush?
+    same_suit?(cards)
+  end
+
+  def straight?
+    sequential?(cards)
+  end
+
+  def three_of_a_kind?
+    groups = card_groups_by(cards, :weight)
+    groups.any? { |weight, cards| cards.size == 3 } 
+  end
+
+  def two_pair?
+    groups = card_groups_by(cards, :weight)
+    sets_of_2_or_more = groups.select { |weight, cards| cards.size >= 2 }
+    sets_of_2_or_more.size == 2
+  end
+
+  def one_pair?
+    card_groups_by(cards, :weight).size < VALID_CARD_COUNT
   end
 
   private
@@ -34,4 +96,16 @@ class Poker::Hand
     end
   end
 
+  def card_groups_by(cards, method = :weight)
+    cards.group_by(&method)
+  end
+
+  def same_suit?(cards)
+    cards.map(&:suit).uniq.size == 1
+  end
+
+  def sequential?(cards)
+    weights = cards.map(&:weight)
+    (weights.min..weights.max).size == VALID_CARD_COUNT
+  end
 end
--- spec/lib/poker/hand_spec.rb
+++ spec/lib/poker/hand_spec.rb
@@ -2,11 +2,16 @@ require 'test_helper'
 
 RSpec.describe Poker::Hand do
 
-  context "#initialize" do
+  describe "#initialize" do
     it "succeeds with a valid array of cards" do
       card_codes = %w(8C TS KC 9H 4S)
       hand = Poker::Hand.new(card_codes)
       expect(hand.cards.map(&:card_code)).to eq(card_codes)
+    end
+
+    it "succeeds with a valid string of cards" do
+      card_codes = "2S 8D 8C 4C TS"
+      expect(Poker::Hand.new(card_codes))
     end
 
     it "fails with an invalid set of cards" do
@@ -26,7 +31,7 @@ RSpec.describe Poker::Hand do
     end
   end
 
-  context "#face_value" do
+  describe "#face_value" do
     it "returns the sum of the face values of all cards" do
       {
         36 => %w(5H 5C 6S 7S KD), #=> 5, 5, 6, 7, 13
@@ -36,5 +41,207 @@ RSpec.describe Poker::Hand do
         expect(Poker::Hand.new(cards).face_value).to eq(val)
       end
     end
+  end
+
+  describe "#high_card" do
+    it "returns the card with the highest weight" do
+      {
+        Poker::Card.new('AS') => %w(KH 4H AS JS QS),
+        Poker::Card.new('TS') => %w(2S 8D 8C 4C TS)
+      }.each do |high_card, cards|
+        hand = Poker::Hand.new(cards)
+        expect(hand.high_card).to eq(high_card)
+      end
+    end
+  end
+
+  context "named hands" do
+    describe "#royal_flush?" do
+      it "returns true for a royal flush" do
+        cards = %w(TS JS QS KS AS)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to be_royal_flush
+      end
+
+      it "returns false if any card is out of suit" do
+        cards = %w(TS JS QH KS AS)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to_not be_royal_flush
+      end
+
+      it "returns false if any card is not a face card" do
+        cards = %w(9S JS QH KS AS)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to_not be_royal_flush
+      end
+    end
+
+    describe "#straight_flush?" do
+      it "returns true for a straight flush" do
+        [
+          %w(9D 8D 7D 6D 5D),
+          %w(QH TH KH 9H JH)
+        ].each do |cards|
+          hand = Poker::Hand.new(cards)
+          expect(hand).to be_straight_flush
+        end
+      end
+
+      it "returns false if any card is not in suit" do
+        cards = %w(9D 8D 7D 6C 5D)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to_not be_straight_flush
+      end
+
+      it "returns false if any card is not sequential" do
+        [
+          %w(2C 3C 4C 5C 7C),
+          %w(AS KS QS JS 3S)
+        ].each do |cards|
+          hand = Poker::Hand.new(cards)
+          expect(hand).to_not be_straight_flush
+        end
+      end
+    end
+
+    describe "#four_of_a_kind?" do
+      it "returns true if the hand contains all identical cards from each suit" do
+        [
+          %w(AS AC AH AD 2C),
+          %w(8C 8D JH 8H 8S)
+        ].each do |cards|
+          hand = Poker::Hand.new(cards)
+          expect(hand).to be_four_of_a_kind
+        end
+      end
+
+      it "returns false if any suit is missing from the set of cards" do
+        cards = %w(AS AC AH 8H 2H)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to_not be_four_of_a_kind
+      end
+    end
+
+    describe "#full_house?" do
+      it "returns true if it contains three of a kind and a pair" do
+        [
+          %w(AC AD AS KH KS),
+          %w(2C 2D 9H 9S 9D)
+        ].each do |cards|
+          hand = Poker::Hand.new(cards)
+          expect(hand).to be_full_house
+        end
+      end
+
+      it "returns false if it does not contain three of a kind and a pair" do
+        cards = %w(AC AD JH KH KS)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to_not be_full_house
+      end
+    end
+
+    describe "#flush" do
+      it "returns true if all cards are the same suit" do
+        cards = %w(2D 9D 7D JD 3D)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to be_flush
+      end
+
+      it "returns false if any card is out of suit" do
+        cards = %w(2D 9D 7C JD 3D)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to_not be_flush
+      end
+    end
+
+    describe "#straight?" do
+      it "returns true if all cards are sequential" do
+        [
+          %w(9D 8H 7C 6S 5D),
+          %w(TS JD QH KC AS),
+          %w(5D 9C 7H 6S 8S)
+        ].each do |cards|
+          hand = Poker::Hand.new(cards)
+          expect(hand).to be_straight
+        end
+      end
+
+      it "returns false is all cards are not sequential" do
+        cards = %w(4D 9C 7H 6S 8S)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to_not be_straight
+      end
+    end
+
+    describe "#three_of_a_kind?" do
+      it "returns true if contains 3 cards with the same face value" do
+        cards = %w(AD AS AH TS 4C)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to be_three_of_a_kind
+      end
+
+      it "returns false if does not contain 3 cards with the same face value" do
+        cards = %w(AD AS TH TC JD)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to_not be_three_of_a_kind
+      end
+    end
+
+    describe "#two_pair" do
+      it "returns true if contains 2 sets of cards with the same face value" do
+        [
+          %w(AD AS TH JC JD),
+          %w(2H 3C 2D 3S 4H)
+        ].each do |cards|
+          hand = Poker::Hand.new(cards)
+          expect(hand).to be_two_pair
+        end
+      end
+
+      it "returns false if does not contain 2 sets of cards with the same face value" do
+        cards = %w(AD AS TH JC 9D)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to_not be_two_pair
+      end
+    end
+
+    describe "#one_pair?" do
+      it "returns true if contains at least one set of cards with the same face value" do
+        [
+          %w(AD AS TH JC 9D),
+          %w(2H 3C 2D 3S 4H)
+        ].each do |cards|
+          hand = Poker::Hand.new(cards)
+          expect(hand).to be_one_pair
+        end
+      end
+
+      it "returns false if does not contain at least on set of cards with the same face value" do
+        cards = %w(2C 3C 4C 5C 6C)
+        hand = Poker::Hand.new(cards)
+        expect(hand).to_not be_one_pair
+      end
+    end
+
+  end
+
+  context "as comparable" do
+    it "royal flush beats straight flush"
+
+    it "straight flush beats four of a kind"
+
+    it "four of a kind beats full house"
+
+    it "full house beats flush"
+
+    it "flush beats straight"
+
+    it "straight beats three of a kind"
+
+    it "three of a kind beats two pairs"
+
+    it "two pairs beats one pair"
+
+    it "one pair beats high card"
   end
 end
