b6419190149d48bbda4cccfbadee362e56dc3d88
Jason Cartwright <jcartwright@Jasons-MBP.fios-router.home>
2017-07-03 11:00:26 -0500

    Implement Game#play

--- lib/poker/game.rb
+++ lib/poker/game.rb
@@ -1,8 +1,62 @@
 module Poker
   class Game
 
-    def initialize(game_file)
-      @all_hands = IO.read(game_file)
+    attr_reader :all_hands
+
+    def initialize(game)
+      # Attempt to open the file, or fallback
+      # to a string containing space-sep cards
+      # with line breaks
+      game_content = game.to_s.end_with?('.txt') ? File.read(game) : game
+      @all_hands = load_game_from_content(game_content)
+
+      # make sure the hands are all valid
+      validate_game!(@all_hands)
+    end
+
+    def play
+      all_hands.map do |play|
+        lh = play[:player_one]
+        rh = play[:player_two]
+
+        begin
+          winner = case
+          when lh > rh then :player_one
+          when rh > lh then :player_two
+          else :tie
+          end
+
+          {
+            winner: winner,
+            player_one: { rank: lh.rank, cards: lh.cards.map(&:card_code) },
+            player_two: { rank: rh.rank, cards: rh.cards.map(&:card_code) }
+          }
+        rescue => e
+          puts e
+          puts lh.cards.map(&:card_code).inspect
+          puts rh.cards.map(&:card_code).inspect
+        end
+      end
+    end
+
+    private
+
+    def load_game_from_content(game_content)
+      game_content.split("\n").map do |line|
+        cards = line.split
+        {
+          player_one: Poker::Hand.new(cards[0..4]),
+          player_two: Poker::Hand.new(cards[5..9])
+        }
+      end
+    end
+
+    def validate_game!(hands)
+      hands.each do |play|
+        # { player_one: <#Poker::Hand>, player_two: <#Poker::Hand> }
+        cards = play.map { |player, hand| hand.cards.map(&:card_code) }.flatten.uniq
+        raise ArgumentError.new("Invalid play #{cards.inspect}") unless cards.size == 10
+      end
     end
   end
 end
--- spec/lib/poker/game_spec.rb
+++ spec/lib/poker/game_spec.rb
@@ -5,16 +5,95 @@ RSpec.describe Poker::Game do
     File.join(File.dirname(__FILE__),
               '../../../spec/fixtures', 'poker.txt')
   }
+  let(:game_string) {
+    <<-GAME
+    5H 5C 6S 7S KD 2C 3S 8S 8D TD
+    5D 8C 9S JS AC 2C 5C 7D 8S QH
+    2D 9C AS AH AC 3D 6D 7D TD QD
+    4D 6S 9H QH QC 3D 6D 7H QD QS
+    2H 2D 4C 4D 4S 3C 3D 3S 9S 9D
+    GAME
+  }
 
-  context "#initialize" do
+  describe "#initialize" do
     it "succeeds with a valid game file" do
-      expect(Poker::Game.new(game_file))
+      expect(Poker::Game.new(game_file)).to be_a(Poker::Game)
+    end
+
+    it "succeeds with a valid list of cards" do
+      expect(Poker::Game.new(game_string)).to be_a(Poker::Game)
     end
 
     it "fails with a missing file" do
       expect {
-        Poker::Game.new('/path/to/missing/file')
+        Poker::Game.new('/path/to/missing/file.txt')
       }.to raise_error(SystemCallError)
+    end
+
+    it "parses game content into a list of hands" do
+      game = Poker::Game.new(game_file)
+      expect(game.all_hands.size).to eq(1000)
+
+      game = Poker::Game.new(game_string)
+      expect(game.all_hands.size).to eq(5)
+    end
+
+    it "fails with an invalid card" do
+      # invalid card code 0H
+      bad_game = "0H 5C 6S 7S KD 2C 3S 8S 8D TD"
+      expect {
+        Poker::Game.new(bad_game)
+      }.to raise_error(ArgumentError)
+    end
+
+    context "fails with an invalid hand" do
+      it "due to duplicate card `5C` in player's hands" do
+        bad_game = "5H 5C 6S 7S KD 5C 3S 8S 8D TD"
+        expect {
+          Poker::Game.new(bad_game)
+        }.to raise_error(ArgumentError)
+      end
+
+      it "due to too few cards" do
+        bad_game = "5C 6S 7S KD 2C 3S 8S 8D TD"
+        expect {
+          Poker::Game.new(bad_game)
+        }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe "#play" do
+    let(:game) { Poker::Game.new(game_string) }
+
+    it "determines the results of each hand" do
+      results = game.play
+      expected = [:player_two, :player_one, :player_two, :player_one, :player_one]
+      actual = results.map { |play| play[:winner] }
+
+      expect(actual).to eq(expected)
+    end
+
+    it "returns the results of the game" do
+      full_game = Poker::Game.new(game_file)
+      results = full_game.play
+
+      puts "\n\n**** RESULTS ****"
+
+      player_one_tally = results.select { |play| play[:winner] == :player_one }
+      puts "Player 1: #{player_one_tally.size}"
+
+      player_two_tally = results.select { |play| play[:winner] == :player_two }
+      puts "Player 2: #{player_two_tally.size}"
+
+      ties = results.select { |play| play[:winner] == :tie }
+      puts "Ties: #{ties.size}"
+
+      puts "*****************\n\n"
+
+      expect(player_one_tally.size).to eq(376)
+      expect(player_two_tally.size).to eq(624)
+      expect(ties.size).to eq(0)
     end
   end
 end
